021f465cd76b621fd2108e082a6bf526f21cf5a2
Louis Maddox <lmmx@users.noreply.github.com>
2025-11-17 11:13:04 +0000

    refactor(ansi): owo_colors-compatible API

--- src/highlight/ansi.rs
+++ src/highlight/ansi.rs
@@ -1,6 +1,7 @@
 //! Minimal ANSI color support
 
 /// ANSI color codes
+#[derive(Clone, Copy)]
 pub struct Color {
     code: u8,
 }
@@ -8,10 +9,6 @@ pub struct Color {
 impl Color {
     pub const fn new(code: u8) -> Self {
         Self { code }
-    }
-
-    pub fn paint(&self, text: &str) -> String {
-        format!("\x1b[{}m{}\x1b[0m", self.code, text)
     }
 }
 
@@ -21,3 +18,14 @@ pub const YELLOW: Color = Color::new(33);
 pub const MAGENTA: Color = Color::new(35);
 pub const GREEN: Color = Color::new(32);
 pub const BLUE: Color = Color::new(34);
+
+/// Extension trait to add `.color()` method to any type that can be displayed
+pub trait Highlight {
+    fn color(&self, color: Color) -> String;
+}
+
+impl<T: std::fmt::Display> Highlight for T {
+    fn color(&self, color: Color) -> String {
+        format!("\x1b[{}m{}\x1b[0m", color.code, self)
+    }
+}
