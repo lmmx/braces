b689426d5cfc7352970c646ba4130a745bcfe899
Louis Maddox <lmmx@users.noreply.github.com>
2025-11-17 11:16:32 +0000

    refactor: allow opt-in owo_colors

--- src/highlight/ansi.rs
+++ src/highlight/ansi.rs
@@ -1,31 +1,48 @@
 //! Minimal ANSI color support
 
-/// ANSI color codes
-#[derive(Clone, Copy)]
-pub struct Color {
-    code: u8,
-}
+#[cfg(not(feature = "owo"))]
+mod internal {
+    /// ANSI color codes
+    #[derive(Clone, Copy)]
+    pub struct Color {
+        code: u8,
+    }
 
-impl Color {
-    pub const fn new(code: u8) -> Self {
-        Self { code }
+    impl Color {
+        pub const fn new(code: u8) -> Self {
+            Self { code }
+        }
+    }
+
+    // Standard colors
+    pub const CYAN: Color = Color::new(36);
+    pub const YELLOW: Color = Color::new(33);
+    pub const MAGENTA: Color = Color::new(35);
+    pub const GREEN: Color = Color::new(32);
+    pub const BLUE: Color = Color::new(34);
+
+    /// Extension trait to add `.color()` method to any type that can be displayed
+    pub trait Highlight {
+        fn color(&self, color: Color) -> String;
+    }
+
+    impl<T: std::fmt::Display> Highlight for T {
+        fn color(&self, color: Color) -> String {
+            format!("\x1b[{}m{}\x1b[0m", color.code, self)
+        }
     }
 }
 
-// Standard colors
-pub const CYAN: Color = Color::new(36);
-pub const YELLOW: Color = Color::new(33);
-pub const MAGENTA: Color = Color::new(35);
-pub const GREEN: Color = Color::new(32);
-pub const BLUE: Color = Color::new(34);
+#[cfg(feature = "owo")]
+mod internal {
+    pub use owo_colors::{colors::*, OwoColorize as Highlight};
+    pub type Color = &'static dyn owo_colors::DynColor;
 
-/// Extension trait to add `.color()` method to any type that can be displayed
-pub trait Highlight {
-    fn color(&self, color: Color) -> String;
+    pub const CYAN: Color = &Cyan;
+    pub const YELLOW: Color = &Yellow;
+    pub const MAGENTA: Color = &Magenta;
+    pub const GREEN: Color = &Green;
+    pub const BLUE: Color = &Blue;
 }
 
-impl<T: std::fmt::Display> Highlight for T {
-    fn color(&self, color: Color) -> String {
-        format!("\x1b[{}m{}\x1b[0m", color.code, self)
-    }
-}
+pub use internal::*;
